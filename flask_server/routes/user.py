17157e77e79265db950a0667003a774d9290a938
eko-develops <ekodevelops@gmail.com>
2023-09-01 13:24:53 -0700

    remove auth method

--- flask_server/routes/user.py
+++ flask_server/routes/user.py
@@ -5,37 +5,6 @@ from flask_server.controllers.user import UserController
 user = Blueprint("user", __name__, url_prefix="/user")
 
 
-@user.route("/register", methods=["POST"])
-def register():
-    body = request.get_json()
-
-    # Ensure all fields are present
-    if not all(key in body for key in ("username", "password", "email")):
-        return jsonify(message="Missing required fields"), 400
-
-    username = body["username"]
-    password = body["password"]
-    email = body["email"]
-
-    result = UserController.register(username, password, email)
-
-    if result["status"] == False:
-        return (
-            jsonify(
-                status=result["status"],
-                user=None,
-                status_code=result["status_code"],
-                message=result["message"],
-            ),
-            result["status_code"],
-        )
-
-    return (
-        jsonify(user=result["user"], message="New user registered successfully."),
-        201,
-    )
-
-
 @user.route("/all", methods=["GET"])
 def all_users():
     users = UserController.get_all()
