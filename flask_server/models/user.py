8b4fdba9ddf54cf06be6c06aa73c691f88c4ec70
ekoret <ekodevelops@gmail.com>
2023-12-20 23:15:58 -0800

    update schema for mysql

--- flask_server/models/user.py
+++ flask_server/models/user.py
@@ -1,7 +1,8 @@
 from flask_server.extensions import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(100))
