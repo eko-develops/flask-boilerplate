2d856760f2c9d8744a407e02e3a0976f2180fc28
ekoret <ekodevelops@gmail.com>
2023-12-20 23:17:33 -0800

    add login route, clean up register route

--- flask_server/routes/auth.py
+++ flask_server/routes/auth.py
@@ -1,8 +1,36 @@
 from flask import Blueprint, jsonify, request
 
-from flask_server.controllers.user import UserController
+from flask_server.controllers.auth import AuthController
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@auth.route("/login", methods=["POST"])
+def login():
+    body = request.get_json()
+
+    # Ensure all fields are present
+    if not all(key in body for key in ("username", "password")):
+        return jsonify(message="Missing required fields"), 400
+
+    username = body["username"]
+    password = body["password"]
+
+    result = AuthController.login(username, password)
+
+    if result["status"] == False:
+        return (
+            jsonify(
+                user=None,
+                message=result["message"],
+            ),
+            result["status_code"],
+        )
+
+    return (
+        jsonify(user=result["user"], message="User logged in successfully."),
+        201,
+    )
 
 
 @auth.route("/register", methods=["POST"])
@@ -17,14 +45,12 @@ def register():
     password = body["password"]
     email = body["email"]
 
-    result = UserController.create(username, password, email)
+    result = AuthController.register(username, password, email)
 
     if result["status"] == False:
         return (
             jsonify(
-                status=result["status"],
                 user=None,
-                status_code=result["status_code"],
                 message=result["message"],
             ),
             result["status_code"],
